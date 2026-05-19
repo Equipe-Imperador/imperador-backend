fba0900984a67855ccfe5e00ed369ec1aa894a02
Nicolas-Rossi28 <nicolasrossigariba@gmail.com>
2026-05-19 18:38:48 -0300

    arrumando

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST || 'db',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'postgres',
+  ssl: process.env.DB_SSL === 'true',
 });
 
 export default pool;
